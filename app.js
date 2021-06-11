7eb30b0e5e6c3873a8360198658a124926f7906f
CaioHTeixeira <caioteixeiraaa@gmail.com>
2021-06-10 21:09:07 -0300

    filter method added

--- app.js
+++ app.js
@@ -5,31 +5,18 @@ const paragraphFeedback = document.querySelector('.paragraphFeedback')
 
 const correctAnswers = ['B', 'B', 'D', 'C', 'A']
 
-const shouldClosePopup = (classNames, clickedElementClassName) => classNames.some(className => clickedElementClassName === className)
+const shouldClosePopup = (classNames, clickedElementClassName) => 
+    classNames.some(className => clickedElementClassName === className)
 
-const getUserAnwers = () => {
-    let userAnswers = []
-
-    correctAnswers.forEach((_, index) => { 
+const getRightAnswersFromUser = () => {
+    const rightAnswersFromUser = correctAnswers.filter((correctAnswer, index) => {
         const userAnswer = form[`inputQuestion${index + 1}`].value
-        userAnswers.push(userAnswer)
+
+        return userAnswer === correctAnswer 
     })
 
-    return userAnswers
+    return rightAnswersFromUser
 }
-
-const rightAnswersFromUser = userAnswers => {
-    let rightAnswers = 0
-    
-    userAnswers.forEach((userAnswer, index) => {
-        const isUserAnswerCorrect = userAnswer === correctAnswers[index]
-        
-        if (isUserAnswerCorrect) {
-            rightAnswers++
-        }
-    })
-    return rightAnswers
-} 
 
 const showScorePopup = (rightAnswers, totalQuestions) => {
     const feedbackQuizMessage = `Você acertou ${rightAnswers} de ${totalQuestions} perguntas!`
@@ -40,10 +27,10 @@ const showScorePopup = (rightAnswers, totalQuestions) => {
 const handleFeedbackQuiz = event => {
     event.preventDefault()
 
-    const userAnswers = getUserAnwers()
+    const rightAnswers = getRightAnswersFromUser().length
+    const totalQuestions = correctAnswers.length
 
-    const rightAnswers = rightAnswersFromUser(userAnswers)
-    showScorePopup(rightAnswers, userAnswers.length) 
+    showScorePopup(rightAnswers, totalQuestions) 
 }
 
 const handlePopupClose = event => { 
